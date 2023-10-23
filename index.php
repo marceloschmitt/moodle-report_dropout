a16b0b7de966fade5bb3428b6edef592cbe86ed8
Marcelo A. Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2023-10-23 16:16:39 -0300

    Update index.php

--- index.php
+++ index.php
@@ -23,10 +23,10 @@
  */
 
 define('NO_OUTPUT_BUFFERING', true);
-$courseid = required_param('id', PARAM_INT);
 
 require('../../config.php');
 require_once($CFG->libdir.'/adminlib.php');
+$courseid = required_param('id', PARAM_INT);
 
 /* Colocar depois o teste de capability */
 
