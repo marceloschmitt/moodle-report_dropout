6d5479849d381cb1f8d8d94f20ac8f4d36c35be6
Marcelo A. Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2023-10-23 16:23:03 -0300

    Create renderer.php

---
+++ classes/output/renderer.php
@@ -0,0 +1,21 @@
+<?php
+// Standard GPL and phpdocs
+namespace report_dropout\output;                                                                                                         
+                                                                                                                                    
+defined('MOODLE_INTERNAL') || die;                                                                                                  
+                                                                                                                                    
+use plugin_renderer_base;  
+
+class renderer extends plugin_renderer_base {
+    /**                                                                                                                             
+     * Defer to template.                                                                                                           
+     *                                                                                                                              
+     * @param index_page $page                                                                                                      
+     *                                                                                                                              
+     * @return string html for the page                                                                                             
+     */                                                                                                                             
+    public function render_index_page($page) {                                                                                      
+        $data = $page->export_for_template($this);                                                                                  
+        return parent::render_from_template('report_dropout/index_page', $data);                                                         
+    }           
+}
