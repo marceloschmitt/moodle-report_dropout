40cb707cc5ef7a2dc35877dcdf14c8eae5456076
Marcelo A. Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2023-10-23 16:35:18 -0300

    Update index.php

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ $PAGE->set_url('/report/dropout/index.php');
 $PAGE->set_context(context_course::instance($courseid));
 $PAGE->set_pagelayout('standard');
 $PAGE->set_title('Nome da disciplina');
-//$PAGE->set_heading(get_string('pluginname', 'report_dropout'));
+$PAGE->set_heading(get_string('pluginname', 'report_dropout'));
 
 $output = $PAGE->get_renderer('report_dropout');
 echo $output->header();
