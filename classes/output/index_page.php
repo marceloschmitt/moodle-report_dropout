54facdc2ee3b994fc7549e25b8f125dd78b72b5d
Marcelo A. Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2023-10-27 13:23:40 -0300

    Update index_page.php

--- classes/output/index_page.php
+++ classes/output/index_page.php
@@ -16,7 +16,6 @@ class index_page implements renderable, templatable {
         $this->students[] = (object) array('name' => 'Marcelo');
         $this->students[] = (object) array('name' => 'Mariana');
         //$this->students = get_enrolled_users($context);
-        echo var_dump($this->students); exit;
     }
                                                                                                                             
     public function export_for_template(renderer_base $output) {                                                                    
