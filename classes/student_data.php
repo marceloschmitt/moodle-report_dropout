f3506fb3cf49a7a2fb0bb83fe936d8510064e983
Marcelo A. Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2024-02-22 08:57:29 -0300

    Update student_data.php

--- classes/student_data.php
+++ classes/student_data.php
@@ -3,7 +3,12 @@
 namespace report_dropout;
 
 class student_data {
-    var $sometext = null;
+    var $userid;
+    var $header = array();
+    var $behaviour1 = array();
 
     public function __construct($userid) {
-        
+        $header = array("AGO", "SET", "OUT", "NOV", "DEZ");
+        $behaviour1 = array(9, 10, 5, 7, 8);
+    }
+}
