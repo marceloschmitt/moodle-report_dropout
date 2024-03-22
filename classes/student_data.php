9a51f740eee63ebf8936642709dbcea5880f8600
Marcelo A. Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2024-03-22 08:51:16 -0300

    Update student_data.php

--- classes/student_data.php
+++ classes/student_data.php
@@ -5,10 +5,73 @@ namespace report_dropout;
 class student_data {
     var $userid;
     var $header = array();
-    var $behaviour1 = array();
+    var $subheader = array();
+    var $behaviourcondition1 = array();
+    var $behaviourcondition2 = array();
+    var $behaviourcondition3 = array();
+    var $behaviourcondition4 = array();
+    var $socialcondition1 = array();
+    var $socialcondition2 = array();
+    var $socialcondition3 = array();
+    var $socialcondition4 = array();
+    var $cognitivecondition1 = array();
+    var $cognitivecondition2 = array();
+    var $cognitivecondition3 = array();
 
     public function __construct($userid) {
-        $header = array("AGO", "SET", "OUT", "NOV", "DEZ");
-        $behaviour1 = array(9, 10, 5, 7, 8);
+	    $contador_temp = 5;
+	    $this->header[] = (object) array('month' => 'ago');
+	    $this->header[] = (object) array('month' => 'set');
+	    $this->header[] = (object) array('month' => 'out');
+	    $this->header[] = (object) array('month' => 'nov');
+	    $this->header[] = (object) array('month' => 'dez');
+	    for ($i = 0; $i < $contador_temp; $i++) {
+	    	$this->subheader[] = (object) array('halfmonth' => 'Q1');
+	    	$this->subheader[] = (object) array('halfmonth' => 'Q2');
+	    }
+	    for ($i = 0; $i < ($contador_temp*2); $i++) {
+	    	$value = rand(0, 10);
+		$this->behaviourindicator1[] = (object) array('value' => $value);
+	    }
+	    for ($i = 0; $i < ($contador_temp*2); $i++) {
+	    	$value = rand(0, 10);
+		$this->behaviourindicator2[] = (object) array('value' => $value);
+	    }
+	    for ($i = 0; $i < ($contador_temp*2); $i++) {
+	    	$value = rand(0, 10);
+		$this->behaviourindicator3[] = (object) array('value' => $value);
+	    }
+	    for ($i = 0; $i < ($contador_temp*2); $i++) {
+	    	$value = rand(0, 10);
+		$this->behaviourindicator4[] = (object) array('value' => $value);
+	    }
+	    for ($i = 0; $i < ($contador_temp*2); $i++) {
+	    	$value = rand(0, 10);
+		$this->socialindicator1[] = (object) array('value' => $value);
+	    }
+	    for ($i = 0; $i < ($contador_temp*2); $i++) {
+	    	$value = rand(0, 10);
+		$this->socialindicator2[] = (object) array('value' => $value);
+	    }
+	    for ($i = 0; $i < ($contador_temp*2); $i++) {
+	    	$value = rand(0, 10);
+		$this->socialindicator3[] = (object) array('value' => $value);
+	    }
+	    for ($i = 0; $i < ($contador_temp*2); $i++) {
+	    	$value = rand(0, 10);
+		$this->socialindicator4[] = (object) array('value' => $value);
+	    }
+	    for ($i = 0; $i < ($contador_temp*2); $i++) {
+	    	$value = rand(0, 10);
+		$this->cognitiveindicator1[] = (object) array('value' => $value);
+	    }
+	    for ($i = 0; $i < ($contador_temp*2); $i++) {
+	    	$value = rand(0, 10);
+		$this->cognitiveindicator2[] = (object) array('value' => $value);
+	    }
+	    for ($i = 0; $i < ($contador_temp*2); $i++) {
+	    	$value = rand(0, 10);
+		$this->cognitiveindicator3[] = (object) array('value' => $value);
+	    }
     }
 }
