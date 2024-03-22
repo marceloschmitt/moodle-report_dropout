bd7481e6829d2f75f769e566a057a9359f9ec9ec
Marcelo A. Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2024-03-22 11:10:06 -0300

    Update version.php

--- version.php
+++ version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die;
   
-$plugin->version   = 2024032201;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2024032202;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2023100400;        // Requires this Moodle version.
 $plugin->component = 'report_dropout';  // Full name of the plugin (used for diagnostics).
