a082be4677c255be396eee06cadafdd46eeb8ce1
Marcelo A. Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2024-03-22 08:52:54 -0300

    Update report_page.php

--- classes/output/report_page.php
+++ classes/output/report_page.php
@@ -11,8 +11,21 @@ class report_page implements renderable, templatable {
     /** @var string $sometext Some text to show how to pass data to a template. */                                                  
     var $sometext = null;                                                                                                           
             
-    public function __construct($sometext) {                                                                                        
-        $this->sometext = $sometext;                                                                                                
+    public function __construct($sometext, $studentdata) {                                                                                        
+	    $this->sometext = $sometext;
+	    $this->header = $studentdata->header;
+	    $this->subheader = $studentdata->subheader;
+	    $this->behaviourindicator1 = $studentdata->behaviourindicator1;
+	    $this->behaviourindicator2 = $studentdata->behaviourindicator2;
+	    $this->behaviourindicator3 = $studentdata->behaviourindicator3;
+	    $this->behaviourindicator4 = $studentdata->behaviourindicator4;
+	    $this->socialindicator1 = $studentdata->socialindicator1;
+	    $this->socialindicator2 = $studentdata->socialindicator2;
+	    $this->socialindicator3 = $studentdata->socialindicator3;
+	    $this->socialindicator4 = $studentdata->socialindicator4;
+	    $this->cognitiveindicator1 = $studentdata->cognitiveindicator1;
+	    $this->cognitiveindicator2 = $studentdata->cognitiveindicator2;
+	    $this->cognitiveindicator3 = $studentdata->cognitiveindicator3;
     }
 
     /**                                                                                                                             
@@ -22,7 +35,20 @@ class report_page implements renderable, templatable {
      */                                                                                                                             
     public function export_for_template(renderer_base $output) {                                                                    
         $data = new stdClass();                                                                                                     
-        $data->sometext = $this->sometext;                                                                                          
+	$data->sometext = $this->sometext;
+	$data->header = $this->header;       
+	$data->subheader = $this->subheader;       
+	$data->behaviourindicator1 = $this->behaviourindicator1;       
+	$data->behaviourindicator2 = $this->behaviourindicator2;       
+	$data->behaviourindicator3 = $this->behaviourindicator3;       
+	$data->behaviourindicator4 = $this->behaviourindicator4;       
+	$data->socialindicator1 = $this->socialindicator1;       
+	$data->socialindicator2 = $this->socialindicator2;       
+	$data->socialindicator3 = $this->socialindicator3;       
+	$data->socialindicator4 = $this->socialindicator4;       
+	$data->cognitiveindicator1 = $this->cognitiveindicator1;       
+	$data->cognitiveindicator2 = $this->cognitiveindicator2;       
+	$data->cognitiveindicator3 = $this->cognitiveindicator3;       
         return $data;                                                                                                               
     }
 }
