d669988d906166e87d8635ba93242a66de5ed54d
larissaSM23 <85263003+larissaSM23@users.noreply.github.com>
2024-04-09 16:13:31 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -179,6 +179,26 @@ $chartScript = "
       var chart = new google.visualization.BubbleChart(document.getElementById('bubble_chart_teste'));
       chart.draw(data, options);
     }
+	"<h3>GRÁFICO BOLHA COM COR</h3>"
+    function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['ID', 'X', 'Y', 'Temperature'],
+          ['',   80,  167,      120],
+          ['',   79,  136,      130],
+          ['',   78,  184,      50],
+          ['',   72,  278,      230],
+          ['',   81,  200,      210],
+          ['',   72,  170,      100],
+          ['',   68,  477,      80]
+        ]);
+
+        var options = {
+          colorAxis: {colors: ['yellow', 'red']}
+        };
+
+        var chart = new google.visualization.BubbleChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
    </script>
 ";
    return $chartScript;
