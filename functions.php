fed771f86347ccca620712d860e9d567c0c2eeb4
larissaSM23 <85263003+larissaSM23@users.noreply.github.com>
2024-04-11 00:23:10 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -105,6 +105,7 @@ $chartScript = "
       google.charts.load('current', {'packages':['gantt']});
       google.charts.load('current', {'packages':['corechart']});  
       google.charts.load('current', {'packages':['bar']});
+      google.charts.load('current', {'packages':['annotationchart']});
       google.charts.setOnLoadCallback(drawChart);
       google.charts.setOnLoadCallback(drawChartSimple);
       google.charts.setOnLoadCallback(drawSeriesChart);
@@ -239,7 +240,39 @@ $chartScript = "
 
       chart.draw(data, options);
     }
+    
+    function drawChart() {
+        var data = new google.visualization.DataTable();
+        data.addColumn('date', 'Date');
+        data.addColumn('number', 'Kepler-22b mission');
+        data.addColumn('string', 'Kepler title');
+        data.addColumn('string', 'Kepler text');
+        data.addColumn('number', 'Gliese 163 mission');
+        data.addColumn('string', 'Gliese title');
+        data.addColumn('string', 'Gliese text');
+        data.addRows([
+          [new Date(2314, 2, 15), 12400, undefined, undefined,
+                                  10645, undefined, undefined],
+          [new Date(2314, 2, 16), 24045, 'Lalibertines', 'First encounter',
+                                  12374, undefined, undefined],
+          [new Date(2314, 2, 17), 35022, 'Lalibertines', 'They are very tall',
+                                  15766, 'Gallantors', 'First Encounter'],
+          [new Date(2314, 2, 18), 12284, 'Lalibertines', 'Attack on our crew!',
+                                  34334, 'Gallantors', 'Statement of shared principles'],
+          [new Date(2314, 2, 19), 8476, 'Lalibertines', 'Heavy casualties',
+                                  66467, 'Gallantors', 'Mysteries revealed'],
+          [new Date(2314, 2, 20), 0, 'Lalibertines', 'All crew lost',
+                                  79463, 'Gallantors', 'Omniscience achieved']
+        ]);
 
+        var chart = new google.visualization.AnnotationChart(document.getElementById('chart_annotation'));
+
+        var options = {
+          displayAnnotations: true
+        };
+
+        chart.draw(data, options);
+      }	 
     
    </script>
 ";
