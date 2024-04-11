d23e974802deb57f811d6f1f0ed434eac127d9dc
larissaSM23 <85263003+larissaSM23@users.noreply.github.com>
2024-04-11 00:16:05 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -154,6 +154,28 @@ $chartScript = "
         chart.draw(data, options);
      }
 
+function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Year', 'Sales', 'Expenses', 'Profit'],
+          ['2014', 1000, 400, 200],
+          ['2015', 1170, 460, 250],
+          ['2016', 660, 1120, 300],
+          ['2017', 1030, 540, 350]
+        ]);
+
+        var options = {
+          chart: {
+            title: 'Company Performance',
+            subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+          },
+          bars: 'horizontal' // Required for Material Bar Charts.
+        };
+
+        var chart = new google.charts.Bar(document.getElementById('barchart'));
+
+        chart.draw(data, google.charts.Bar.convertOptions(options));
+      }
+      
      function drawSeriesChart() {
 
       var data = google.visualization.arrayToDataTable([
@@ -182,27 +204,7 @@ $chartScript = "
       chart.draw(data, options);
     }
 
-    function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Year', 'Sales', 'Expenses', 'Profit'],
-          ['2014', 1000, 400, 200],
-          ['2015', 1170, 460, 250],
-          ['2016', 660, 1120, 300],
-          ['2017', 1030, 540, 350]
-        ]);
-
-        var options = {
-          chart: {
-            title: 'Company Performance',
-            subtitle: 'Sales, Expenses, and Profit: 2014-2017',
-          },
-          bars: 'horizontal' // Required for Material Bar Charts.
-        };
-
-        var chart = new google.charts.Bar(document.getElementById('barchart'));
-
-        chart.draw(data, google.charts.Bar.convertOptions(options));
-      }
+    
 
   function daysToMilliseconds(days) {
       return days * 24 * 60 * 60 * 1000;
