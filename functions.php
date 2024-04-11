77739fa8e407597680c036f4c36c068b439b4ca8
larissaSM23 <85263003+larissaSM23@users.noreply.github.com>
2024-04-10 23:11:27 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -202,7 +202,7 @@ $chartScript = "
 
       function drawChart() {
       var data = google.visualization.arrayToDataTable([
-        ["Element", "Density", { role: "style" } ],
+
         ["Copper", 8.94, "#b87333"],
         ["Silver", 10.49, "silver"],
         ["Gold", 19.30, "gold"],
