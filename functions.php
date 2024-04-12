c66d76044e723d395851a64e7d70ac1a122809ee
larissaSM23 <85263003+larissaSM23@users.noreply.github.com>
2024-04-12 02:38:36 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -110,6 +110,7 @@ $chartScript = "
       google.charts.load('current', {'packages':['geochart']});
       google.charts.load('current', {'packages':['sankey']});
       google.charts.load('current', {'packages':['table']});
+      google.charts.load('current', {'packages':['treemap']});
 
       google.charts.setOnLoadCallback(drawChart);
       google.charts.setOnLoadCallback(drawChartGantt);
@@ -130,6 +131,7 @@ $chartScript = "
       google.charts.setOnLoadCallback(drawChartDisp);
       google.charts.setOnLoadCallback(drawChartDegr);
       google.charts.setOnLoadCallback(drawChartTable);
+      google.charts.setOnLoadCallback(drawChartTreemap);
 
      function drawChart() {
         var data = google.visualization.arrayToDataTable([
@@ -603,6 +605,52 @@ $chartScript = "
         var table = new google.visualization.Table(document.getElementById('chartTable'));
 
         table.draw(data, {showRowNumber: true, width: '100%', height: '100%'});
+      }
+
+      function drawChartTreemap() {
+        var data = google.visualization.arrayToDataTable([
+          ['Location', 'Parent', 'Market trade volume (size)', 'Market increase/decrease (color)'],
+          ['Global',    null,                 0,                               0],
+          ['America',   'Global',             0,                               0],
+          ['Europe',    'Global',             0,                               0],
+          ['Asia',      'Global',             0,                               0],
+          ['Australia', 'Global',             0,                               0],
+          ['Africa',    'Global',             0,                               0],
+          ['Brazil',    'America',            11,                              10],
+          ['USA',       'America',            52,                              31],
+          ['Mexico',    'America',            24,                              12],
+          ['Canada',    'America',            16,                              -23],
+          ['France',    'Europe',             42,                              -11],
+          ['Germany',   'Europe',             31,                              -2],
+          ['Sweden',    'Europe',             22,                              -13],
+          ['Italy',     'Europe',             17,                              4],
+          ['UK',        'Europe',             21,                              -5],
+          ['China',     'Asia',               36,                              4],
+          ['Japan',     'Asia',               20,                              -12],
+          ['India',     'Asia',               40,                              63],
+          ['Laos',      'Asia',               4,                               34],
+          ['Mongolia',  'Asia',               1,                               -5],
+          ['Israel',    'Asia',               12,                              24],
+          ['Iran',      'Asia',               18,                              13],
+          ['Pakistan',  'Asia',               11,                              -52],
+          ['Egypt',     'Africa',             21,                              0],
+          ['S. Africa', 'Africa',             30,                              43],
+          ['Sudan',     'Africa',             12,                              2],
+          ['Congo',     'Africa',             10,                              12],
+          ['Zaire',     'Africa',             8,                               10]
+        ]);
+
+        tree = new google.visualization.TreeMap(document.getElementById('chartTreemap'));
+
+        tree.draw(data, {
+          minColor: '#f00',
+          midColor: '#ddd',
+          maxColor: '#0d0',
+          headerHeight: 15,
+          fontColor: 'black',
+          showScale: true
+        });
+
       }
    </script>
 ";
