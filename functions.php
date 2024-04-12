3639ec55af0a4f78ec07e467662947314e79a32f
larissaSM23 <85263003+larissaSM23@users.noreply.github.com>
2024-04-12 00:29:14 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -117,6 +117,7 @@ $chartScript = "
       google.charts.setOnLoadCallback(drawChartColumn);
       google.charts.setOnLoadCallback(drawChartComb);
       google.charts.setOnLoadCallback(drawChartDif);
+      google.charts.setOnLoadCallback(drawChartPizza);
 
      function drawChart() {
         var data = google.visualization.arrayToDataTable([
@@ -376,6 +377,26 @@ $chartScript = "
     var diffData = chartDiff.computeDiff(oldData, newData);
     chartDiff.draw(diffData, options);
   }
+
+  function drawChartPizza() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Work',     11],
+          ['Eat',      2],
+          ['Commute',  2],
+          ['Watch TV', 2],
+          ['Sleep',    7]
+        ]);
+
+        var options = {
+          title: 'My Daily Activities'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('chartPizza'));
+
+        chart.draw(data, options);
+      }
    </script>
 ";
    return $chartScript;
