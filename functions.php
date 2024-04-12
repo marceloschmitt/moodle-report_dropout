54f950104149ece7c59b4b7c7c07e2e2a82057e2
larissaSM23 <85263003+larissaSM23@users.noreply.github.com>
2024-04-12 02:55:05 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -111,7 +111,8 @@ $chartScript = "
       google.charts.load('current', {'packages':['sankey']});
       google.charts.load('current', {'packages':['table']});
       google.charts.load('current', {'packages':['treemap']});
-
+      google.charts.load('upcoming', {'packages': ['vegachart']});
+      
       google.charts.setOnLoadCallback(drawChart);
       google.charts.setOnLoadCallback(drawChartGantt);
       google.charts.setOnLoadCallback(drawChartLine);
@@ -133,6 +134,7 @@ $chartScript = "
       google.charts.setOnLoadCallback(drawChartTable);
       google.charts.setOnLoadCallback(drawChartTreemap);
       google.charts.setOnLoadCallback(drawChartLineTend);
+      google.charts.setOnLoadCallback(drawChartVeg);
 
      function drawChart() {
         var data = google.visualization.arrayToDataTable([
@@ -686,6 +688,109 @@ $chartScript = "
 
         var chartExponential = new google.visualization.ScatterChart(document.getElementById('chartExponential'));
         chartExponential.draw(data, options);
+      }
+
+      function drawChartVeg() {
+        const dataTable = new google.visualization.DataTable();
+        dataTable.addColumn({type: 'string', 'id': 'category'});
+        dataTable.addColumn({type: 'number', 'id': 'amount'});
+        dataTable.addRows([
+          ['A', 28],
+          ['B', 55],
+          ['C', 43],
+          ['D', 91],
+          ['E', 81],
+          ['F', 53],
+          ['G', 19],
+          ['H', 87],
+        ]);
+
+        const options = {
+          'vega': {
+            '$schema': 'https://vega.github.io/schema/vega/v4.json',
+            'width': 500,
+            'height': 200,
+            'padding': 5,
+
+            'data': [{'name': 'table', 'source': 'datatable'}],
+
+            'signals': [
+              {
+                'name': 'tooltip',
+                'value': {},
+                'on': [
+                  {'events': 'rect:mouseover', 'update': 'datum'},
+                  {'events': 'rect:mouseout',  'update': '{}'}
+                ]
+              }
+            ],
+
+            'scales': [
+              {
+                'name: 'xscale',
+                'type': 'band',
+                'domain': {'data': 'table', 'fiel': 'category'},
+                'range': 'width',
+                'padding': 0.05,
+                'round': true
+              },
+              {
+                'name': 'yscale',
+                'domain': {'data': 'table', 'field': 'amount'},
+                'nice': true,
+                'range': 'height'
+              }
+            ],
+
+            'axes': [
+              { 'orient': 'bottom', 'scale': 'xscale' },
+              { 'orient': 'left', 'scale': 'yscale' }
+            ],
+
+            'marks': [
+              {
+                'type': 'rect',
+                'from': {'data':'table'},
+                'encode': {
+                  'enter': {
+                    'x': {'scale': 'xscale', 'field': 'category'},
+                    'width': {'scale': 'xscale', 'band': 1},
+                    'y': {'scale': 'yscale', 'field': 'amount'},
+                    'y2': {'scale': 'yscale', 'value': 0}
+                  },
+                  'update': {
+                    'fill': {'value': 'steelblue'}
+                  },
+                  'hover': {
+                    'fill': {'value': 'red'}
+                  }
+                }
+              },
+              {
+                'type': 'text',
+                'encode': {
+                  'enter': {
+                    'align': {'value': 'center'},
+                    'baseline': {'value': 'bottom'},
+                    'fill': {'value': '#333'}
+                  },
+                  'update': {
+                    'x': {'scale': 'xscale', 'signal': 'tooltip.category', 'band': 0.5},
+                    'y': {'scale': 'yscale', 'signal': 'tooltip.amount', 'offset': -2},
+                    'text': {'signal': 'tooltip.amount'},
+                    'fillOpacity': [
+                      {'test': 'datum === tooltip', 'value': 0},
+                      {'value': 1}
+                    ]
+                  }
+                }
+              }
+            ]
+          }
+        };
+
+        const chart = new google.visualization.VegaChart(document.getElementById('chartVeg'));
+        chart.draw(dataTable, options);
       }
    </script>
 ";
