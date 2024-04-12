1bdb0b50805860567c23bd3be1515ca6ca005d2f
larissaSM23 <85263003+larissaSM23@users.noreply.github.com>
2024-04-12 00:00:13 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -279,6 +279,25 @@ $chartScript = "
 
         chart.draw(data, options);
       }
+
+      function drawChartCandle() {
+    var data = google.visualization.arrayToDataTable([
+      ['Mon', 20, 28, 38, 45],
+      ['Tue', 31, 38, 55, 66],
+      ['Wed', 50, 55, 77, 80],
+      ['Thu', 77, 77, 66, 50],
+      ['Fri', 68, 66, 22, 15]
+      // Treat first row as data as well.
+    ], true);
+
+    var options = {
+      legend:'none'
+    };
+
+    var chart = new google.visualization.CandlestickChart(document.getElementById('chartCandle'));
+
+    chart.draw(data, options);
+  }
     
    </script>
 ";
