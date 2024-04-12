02ebc43bd9fa715e31f869d76c28502b252aa341
larissaSM23 <85263003+larissaSM23@users.noreply.github.com>
2024-04-12 02:21:59 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -525,9 +525,9 @@ $chartScript = "
 
         // For each orgchart box, provide the name, manager, and tooltip to show.
         data.addRows([
-          [{'v':'Mike', 'f':'Mike<div style="color:red; font-style:italic">President</div>'},
+          [{'v':'Mike', 'f':'Mike<div style='color:red; font-style:italic'>President</div>'},
            '', 'The President'],
-          [{'v':'Jim', 'f':'Jim<div style="color:red; font-style:italic">Vice President</div>'},
+          [{'v':'Jim', 'f':'Jim<div style='color:red; font-style:italic'>Vice President</div>'},
            'Mike', 'VP'],
           ['Alice', 'Mike', ''],
           ['Bob', 'Jim', 'Bob Sponge'],
