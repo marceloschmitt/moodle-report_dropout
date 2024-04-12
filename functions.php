a8bc0748a59be638eb0b27c292f1f11b03fb0bb8
larissaSM23 <85263003+larissaSM23@users.noreply.github.com>
2024-04-12 00:10:15 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -310,20 +310,20 @@ $chartScript = "
 
       var view = new google.visualization.DataView(data);
       view.setColumns([0, 1,
-                       { calc: "stringify",
+                       { calc: 'stringify',
                          sourceColumn: 1,
-                         type: "string",
-                         role: "annotation" },
+                         type: 'string',
+                         role: 'annotation'},
                        2]);
 
       var options = {
-        title: "Density of Precious Metals, in g/cm^3",
+        title: 'Density of Precious Metals, in g/cm^3',
         width: 600,
         height: 400,
-        bar: {groupWidth: "95%"},
-        legend: { position: "none" },
+        bar: {groupWidth: '95%'},
+        legend: { position: 'none' },
       };
-      var chart = new google.visualization.ColumnChart(document.getElementById("chartColumn"));
+      var chart = new google.visualization.ColumnChart(document.getElementById('chartColumn'));
       chart.draw(view, options);
   }
     
