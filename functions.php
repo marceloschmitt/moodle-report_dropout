4e2008a7987aff4c3299f854c8cf667e4948b558
larissaSM23 <85263003+larissaSM23@users.noreply.github.com>
2024-04-12 00:12:20 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -115,7 +115,7 @@ $chartScript = "
       google.charts.setOnLoadCallback(drawChartAnnotation);
       google.charts.setOnLoadCallback(drawChartCandle);
       google.charts.setOnLoadCallback(drawChartColumn);
-
+      google.charts.setOnLoadCallback(drawChartComb);
 
      function drawChart() {
         var data = google.visualization.arrayToDataTable([
@@ -326,6 +326,29 @@ $chartScript = "
       var chart = new google.visualization.ColumnChart(document.getElementById('chartColumn'));
       chart.draw(view, options);
   }
+
+  function drawChartComb() {
+        // Some raw data (not necessarily accurate)
+        var data = google.visualization.arrayToDataTable([
+          ['Month', 'Bolivia', 'Ecuador', 'Madagascar', 'Papua New Guinea', 'Rwanda', 'Average'],
+          ['2004/05',  165,      938,         522,             998,           450,      614.6],
+          ['2005/06',  135,      1120,        599,             1268,          288,      682],
+          ['2006/07',  157,      1167,        587,             807,           397,      623],
+          ['2007/08',  139,      1110,        615,             968,           215,      609.4],
+          ['2008/09',  136,      691,         629,             1026,          366,      569.6]
+        ]);
+
+        var options = {
+          title : 'Monthly Coffee Production by Country',
+          vAxis: {title: 'Cups'},
+          hAxis: {title: 'Month'},
+          seriesType: 'bars',
+          series: {5: {type: 'line'}}
+        };
+
+        var chart = new google.visualization.ComboChart(document.getElementById('chartComb'));
+        chart.draw(data, options);
+      }
     
    </script>
 ";
