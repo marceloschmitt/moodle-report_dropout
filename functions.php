778c7f1bf2772c9526df2fbb23adbf995300e3e6
larissaSM23 <85263003+larissaSM23@users.noreply.github.com>
2024-04-12 02:41:47 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -132,6 +132,7 @@ $chartScript = "
       google.charts.setOnLoadCallback(drawChartDegr);
       google.charts.setOnLoadCallback(drawChartTable);
       google.charts.setOnLoadCallback(drawChartTreemap);
+      google.charts.setOnLoadCallback(drawChartLineTend);
 
      function drawChart() {
         var data = google.visualization.arrayToDataTable([
@@ -651,6 +652,40 @@ $chartScript = "
           showScale: true
         });
 
+      }
+
+      function drawChartLineTend() {
+        var data = google.visualization.arrayToDataTable([
+          ['Age', 'Weight'],
+          [ 8,      12],
+          [ 4,      5.5],
+          [ 11,     14],
+          [ 4,      5],
+          [ 3,      3.5],
+          [ 6.5,    7]
+        ]);
+
+        var options = {
+          hAxis: {minValue: 0, maxValue: 15},
+          vAxis: {minValue: 0, maxValue: 15},
+          chartArea: {width:'50%'},
+          trendlines: {
+            0: {
+              type: 'linear',
+              showR2: true,
+              visibleInLegend: true
+            }
+          }
+        };
+
+        var chartLinear = new google.visualization.ScatterChart(document.getElementById('chartLinear'));
+        chartLinear.draw(data, options);
+
+        options.trendlines[0].type = 'exponential';
+        options.colors = ['#6F9654'];
+
+        var chartExponential = new google.visualization.ScatterChart(document.getElementById('chartExponential'));
+        chartExponential.draw(data, options);
       }
    </script>
 ";
