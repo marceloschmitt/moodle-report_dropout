2f740212e0503e37bea7ef8214c7735e271ee9f4
larissaSM23 <85263003+larissaSM23@users.noreply.github.com>
2024-04-12 02:21:22 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -125,6 +125,7 @@ $chartScript = "
       google.charts.setOnLoadCallback(drawChartGeo);
       google.charts.setOnLoadCallback(drawChartHist);
       google.charts.setOnLoadCallback(drawChartArea);
+      google.charts.setOnLoadCallback(drawChartOrganiz);
 
      function drawChart() {
         var data = google.visualization.arrayToDataTable([
@@ -515,7 +516,29 @@ $chartScript = "
         var chart = new google.visualization.AreaChart(document.getElementById('chartArea'));
         chart.draw(data, options);
       }
-      
+
+      function drawChartOrganiz() {
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Name');
+        data.addColumn('string', 'Manager');
+        data.addColumn('string', 'ToolTip');
+
+        // For each orgchart box, provide the name, manager, and tooltip to show.
+        data.addRows([
+          [{'v':'Mike', 'f':'Mike<div style="color:red; font-style:italic">President</div>'},
+           '', 'The President'],
+          [{'v':'Jim', 'f':'Jim<div style="color:red; font-style:italic">Vice President</div>'},
+           'Mike', 'VP'],
+          ['Alice', 'Mike', ''],
+          ['Bob', 'Jim', 'Bob Sponge'],
+          ['Carol', 'Bob', '']
+        ]);
+
+        // Create the chart.
+        var chart = new google.visualization.OrgChart(document.getElementById('chartOrg'));
+        // Draw the chart, setting the allowHtml option to true for the tooltips.
+        chart.draw(data, {'allowHtml':true});
+      }
    </script>
 ";
    return $chartScript;
