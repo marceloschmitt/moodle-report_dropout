867e7436b5c2bbd71c8e570e9f90388412fe5eda
Marcelo A. Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2024-06-04 14:36:49 -0300

    Update student_data.php

--- classes/student_data.php
+++ classes/student_data.php
@@ -117,13 +117,13 @@ class student_data {
         
         for($j = 1; $j < 4; $j++) {
             $this->behaviourtable[] = array_column($this->behaviourconditions, $j);
+                    echo var_dump($this->allconditions). "<BR><BR>";
+                echo var_dump($this->behaviourtable). "<BR><BR>";
+        exit;
             $this->behaviourtable[$index][$i+1] = 
 	            $this->get_behaviour_risk($j+1, array_sum(array_slice($this->behaviourtable[$index], 1)), $i);
 		    $index++;
              $this->behaviourtable = $this->allconditions;
-        echo var_dump($this->allconditions). "<BR><BR>";
-                echo var_dump($this->behaviourtable). "<BR><BR>";
-        exit;
         }
 
 
