731f004be53189f304b03227a864f0e59eabcd84
Marcelo A. Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2024-06-04 15:13:47 -0300

    Update student_data.php

--- classes/student_data.php
+++ classes/student_data.php
@@ -116,10 +116,19 @@ class student_data {
        
        
         $this->behaviourtable[0] = $this->allconditions[0];
-        for($j = 1; $j < 4; $j++) { 
+        $this->behaviourtable[0][0] = get_string('behaviourindicators', 'report_dropout');
+        for($row = 1; $j < 4; $row++) { 
             $this->behaviourtable[] = array_column($this->behaviourconditions, $j);
-            $this->behaviourtable[$j][$i+1] = 
-                    $this->get_behaviour_risk($j+1, array_sum(array_slice($this->behaviourtable[$j], 1)), $i);
+            $this->behaviourtable[$row][$i+1] = 
+                    $this->get_behaviour_risk($row+1, array_sum(array_slice($this->behaviourtable[$row], 1)), $i);
+        }  
+
+        $this->socialtable[0] = $this->allconditions[0];
+        $this->social[0][0] = get_string('socialindicators', 'report_dropout');
+        for($row = 1; $j < 4; $row++) { 
+            $this->socialtable[] = array_column($this->socialconditions, $j);
+            $this->socialtable[$row][$i+1] = 
+                    $this->get_social_risk($row, array_sum(array_slice($this->socialtable[$row], 1)), $i);
         }  
 
 
