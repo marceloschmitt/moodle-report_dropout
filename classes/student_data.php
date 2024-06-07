644e9ee8547c1704f3213dec2c0d2c8a8a0ac06a
Marcelo Schmitt <mschmitt@intelliboard.net>
2024-06-07 15:06:49 -0300

    Developing.

--- classes/student_data.php
+++ classes/student_data.php
@@ -132,7 +132,7 @@ class student_data
         $behaviour2sum = array_sum(array_slice($this->behaviourtable[1], 1));
         $behaviour3sum = array_sum(array_slice($this->behaviourtable[2], 1));
         $behaviour4sum = array_sum(array_slice($this->behaviourtable[3], 1));
-        echo $behaviour2sum . "<br>" . $numberoffortnights; exit;
+        echo count($table[1]) .  $behaviour2sum . "<br>" . $numberoffortnights; exit;
         $avarage = $behaviour2sum / $numberoffortnights;
         if ($avarage >= 7) {
             return '<span class="badge badge-primary">' . get_string('lowrisk', 'report_dropout') . '</span>';
