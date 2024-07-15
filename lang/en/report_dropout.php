e98b302f6f21601a433fc46b228f9c71bc29a2e4
marceloschmitt <marcelo.schmitt@poa.ifrs.edu.br>
2024-07-15 15:40:24 -0300

    Update

--- lang/en/report_dropout.php
+++ lang/en/report_dropout.php
@@ -22,7 +22,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['pluginname'] = 'Dropout report';
+$string['pluginname'] = 'DropStop';
 
 $string['program'] = 'Curso: ';
 $string['class'] = 'Turma: ';
