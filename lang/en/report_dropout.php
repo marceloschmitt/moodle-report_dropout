486eb5e6eec5fd1496992611b31352aaaaf1e946
Marcelo A. Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2024-07-15 14:22:51 -0300

    Update report_dropout.php

--- lang/en/report_dropout.php
+++ lang/en/report_dropout.php
@@ -55,6 +55,8 @@ $string['veryhighrisk'] = 'Risco altíssimo';
 
 // Titles of report_dropout template
 $string['programinformation'] = '1 - Informações gerais';
+$string['programinformation2'] = '1 - Informações gerais';
+
 $string['studentinformation'] = '2 - Informações do aluno';
 $string['graphs'] = '3 - Gráficos';
 $string['studentname'] = 'Nome do aluno';
