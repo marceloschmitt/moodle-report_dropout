787c96ecce5503fb6b691435244982f8d5fee90d
larissaSM23 <85263003+larissaSM23@users.noreply.github.com>
2024-07-24 16:07:05 -0300

    Update version.php

--- version.php
+++ version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2024072303;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version = 2024072304;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires = 2023100400;        // Requires this Moodle version.
 $plugin->component = 'report_dropout';  // Full name of the plugin (used for diagnostics).
