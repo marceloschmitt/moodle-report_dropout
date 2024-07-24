fd7f2b86308a8bc3084a9d10245d601e1ed697b9
larissaSM23 <85263003+larissaSM23@users.noreply.github.com>
2024-07-24 15:13:36 -0300

    Update report_dropout.php

--- lang/en/report_dropout.php
+++ lang/en/report_dropout.php
@@ -70,6 +70,6 @@ $string['nonmandatorycourses'] = 'Disciplinas não obrigatórias no semestre';
 $string['schedule'] = 'Acompanhamento ao cronograma';
 $string['notready'] = 'depende de integração futura';
 
-$string['tabletest'] = 'Tabela';
+$string['situation2'] = 'Tabela';
 //$string['linegraph'] = 'Gráfico de linhas';
 //$string['bargraph'] = 'Gráfico de Barras';
